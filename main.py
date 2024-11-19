0795a26374a1235fd7f9e885b12a9c904ee45c4a
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2024-11-19 23:09:59 +0000

    Add section "Importing and exporting a CSV"

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import math
 import numpy as np
+import pandas as pd
 
 ########################################################################################
 # NumPy: the absolute basics for beginners                                             #
@@ -514,3 +515,41 @@ print(unique_rows)
 #  [ 9 10 11 12]]
 print(indices)  # [0 1 2]
 print(occurrence_count)  # [2 1 1]
+
+########################################################################################
+# Importing and exporting a CSV                                                        #
+########################################################################################
+print("\nImporting and exporting a CSV\n")
+
+# It's simple to read in a CSV that contains existing information. The best and easiest
+# way to do this is to use Pandas.
+x_1 = pd.read_csv('music.csv', header=0).values
+print(x_1)
+
+# You can also simply select the columns you need:
+x_2 = pd.read_csv('music.csv', usecols=['Artist', 'Plays']).values
+print(x_2)
+
+# It's simple to use Pandas in order to export your array as well.
+a_15 = np.array([[-2.58289208,  0.43014843, -1.24082018, 1.59572603],
+                 [ 0.99027828, 1.17150989,  0.94125714, -0.14692469],
+                 [ 0.76989341,  0.81299683, -0.95068423, 0.11769564],
+                 [ 0.20484034,  0.34784527,  1.96979195, 0.51992837]])
+df = pd.DataFrame(a_15)
+print(df)
+#           0         1         2         3
+# 0 -2.582892  0.430148 -1.240820  1.595726
+# 1  0.990278  1.171510  0.941257 -0.146925
+# 2  0.769893  0.812997 -0.950684  0.117696
+# 3  0.204840  0.347845  1.969792  0.519928
+df.to_csv('pd.csv')
+print(pd.read_csv('pd.csv'))
+#    Unnamed: 0         0         1         2         3
+# 0           0 -2.582892  0.430148 -1.240820  1.595726
+# 1           1  0.990278  1.171510  0.941257 -0.146925
+# 2           2  0.769893  0.812997 -0.950684  0.117696
+# 3           3  0.204840  0.347845  1.969792  0.519928
+
+# You can also save your array with the NumPy `savetxt` method. Or you can open the file
+# `np.csv` any time with a text editor!
+np.savetxt('np.csv', a_15, fmt='%.2f', delimiter=',', header='1,  2,  3,  4')
